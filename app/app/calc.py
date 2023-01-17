9c7e272a377dc92e5f6215df1b570775453e7a8f
MarcoRSousaJr <marcorsousajr@gmail.com>
2023-01-17 17:42:42 -0500

    Adding Elementary Test Cases

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,9 +4,9 @@ Calculator Functions For Learning about Test Cases.
 
 
 def add(x,y):
-    """Add x+y"""
+    """Add x+y and return result."""
     return x+y
 
 def subtract(x,y):
     """Subtract x from y and return result."""
-    return x-y
+    return y-x
