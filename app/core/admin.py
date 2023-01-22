a195993ba81c43ce38cf6856e59410be11708dfc
MarcoRSousaJr <marcorsousajr@gmail.com>
2023-01-21 20:56:41 -0500

    admin create user use tuple not set

--- app/core/admin.py
+++ app/core/admin.py
@@ -30,7 +30,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': {
+            'fields': (
                 'email',
                 'password1',
                 'password2',
@@ -38,7 +38,7 @@ class UserAdmin(BaseUserAdmin):
                 'is_active',
                 'is_staff',
                 'is_superuser',
-            }
+            ),
         }),
     )
 
