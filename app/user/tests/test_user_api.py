56be28f591752d8e6fe094800bae8112027e28bd
MarcoRSousaJr <marcorsousajr@gmail.com>
2023-01-22 18:55:36 -0500

    more fixing for create user api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -28,7 +28,7 @@ class PublicUserApiTests(TestCase):
         payload = {
             'email': 'test@example.com',
             'password': 'testpass123',
-            'name': 'Test Name'
+            'name': 'Test Name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
@@ -42,7 +42,7 @@ class PublicUserApiTests(TestCase):
         payload = {
             'email': 'test@example.com',
             'password': 'testpass123',
-            'name': 'Test name',
+            'name': 'Test Name',
         }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
@@ -54,7 +54,7 @@ class PublicUserApiTests(TestCase):
         payload = {
             'email': 'test@example.com',
             'password': 'pw',
-            'name': 'Test Name'
+            'name': 'Test name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
