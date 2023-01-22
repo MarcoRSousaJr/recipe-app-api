5040989aa7cc11ff00e5d21908bb2e2d21e7ca12
MarcoRSousaJr <marcorsousajr@gmail.com>
2023-01-22 18:45:01 -0500

    working on creating user api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -28,7 +28,7 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass123',
             'name': 'Test Name'
         }
-        res = self.client.post(CREATUE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
