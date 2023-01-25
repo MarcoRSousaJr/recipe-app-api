4df3dd9dc42cc2e42a7c96b5a1b60d8a38659221
MarcoRSousaJr <marcorsousajr@gmail.com>
2023-01-24 20:30:06 -0500

    testing and implementing token api update

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -78,13 +78,13 @@ class PublicUserApiTests(TestCase):
             'email': user_details['email'],
             'password': user_details['password']
         }
-        res = self.client.post(TOKENURL, payload)
+        res = self.client.post(TOKEN_URL, payload)
 
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_create_token_bad_credentials(self):
-        """Test returns error if credentials if credentials invalid."""
+        """Test returns error if credentials invalid."""
         create_user(email='test@example.com', password='goodpass')
 
         payload = {'email': 'test@example.com', 'password': 'badpass'}
